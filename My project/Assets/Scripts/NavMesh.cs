dcde52ffa1bd58fb647a1d5cef8fa9b093aba64a
ssjuego <Sanketh@juegostudioz.com>
2023-03-09 14:54:32 +0530

    Basic setup and maze

---
+++ My project/Assets/Scripts/NavMesh.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+    [SerializeField] Transform location;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = location.position;
+    }
+}
