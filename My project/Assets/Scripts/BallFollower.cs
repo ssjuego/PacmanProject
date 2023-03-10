9f5afe397a239320c3516744a80297945850d0de
ssjuego <Sanketh@juegostudioz.com>
2023-03-10 16:53:50 +0530

    Maze

---
+++ My project/Assets/Scripts/BallFollower.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollower : MonoBehaviour
+{
+    public GameObject ballSphere;
+
+    private Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - ballSphere.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+     transform.position = distance + ballSphere.transform.position;
+    }
+}
---
+++ My project/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField] float playerSpeed = 5;
+   // [SerializeField] float rotateSpeed = 90;
+    
+    
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Quaternion rotation = transform.rotation;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+       // transform.rotation = new Quaternion(transform.rotation.x * horizontalInput, transform.rotation.x * horizontalInput, transform.rotation.z * verticalInput, 90f);
+        rigidBody.velocity = new Vector3(horizontalInput * playerSpeed, rigidBody.velocity.y, verticalInput * playerSpeed);
+
+      //  float rotateAmount = Input.GetAxis("Rotation") * rotateSpeed * Time.deltaTime;
+        //Quaternion rotateDelta = Quaternion.Euler(0, rotateAmount, 0);
+       // rotation *= rotateDelta;         // Set the new rotation of the cube
+        //transform.rotation = rotation;
+    }
+}
